1a4cc4bf310ec98b07317912dc9d2c3cd5018261
08160130 <80247079+08160130@users.noreply.github.com>
2021-03-29 14:50:29 +0800

    Create Week06_03.cpp

---
+++ Week06_03.cpp
@@ -0,0 +1,39 @@
+##加手臂
+
+#include <GL/glut.h>
+#include <stdio.h>
+float angle=0;
+void hand()
+{
+    glPushMatrix();
+        glScalef(0.5,0.1,0.1);
+        glutSolidCube(1);
+    glPopMatrix();
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); ///清空矩陣
+
+    glPushMatrix();///備份矩陣
+
+        glTranslatef(-0.25,0,0);///把正確轉動的手臂掛在肩膀
+        glRotatef(angle++,0,0,1);///對Z軸旋轉
+        glTranslatef(-0.25,0,0);///把旋轉軸放正中心
+        hand();
+
+    glPopMatrix();
+    hand();
+    glutSwapBuffers();
+}
+
+
+int main(int argc,char**argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH );
+    glutCreateWindow("Week05 drawing");
+
+    glutDisplayFunc(display);
+    glutIdleFunc(display); ///Idle很閒的時候，就重複畫面
+    glutMainLoop();
+}
