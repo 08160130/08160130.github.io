782dd4ea3b943546aebd3d45cb8d7cdd2fc0dd61
08160130 <80247079+08160130@users.noreply.github.com>
2021-05-24 14:52:56 +0800

    Create Week_14_timer_4.cpp

---
+++ Week_14_timer_4.cpp
@@ -0,0 +1,32 @@
+#include <GL/glut.h>
+int angle=0;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+        glRotatef(angle,0,0,1);
+        glutSolidCube(0.3);
+    glPopMatrix();
+    glutSwapBuffers();
+}
+int diff=2;
+void timer(int t)   ///鬧鐘響後timer叫了
+{
+    glutTimerFunc(30,timer,t+1);   ///再設下一個鬧鐘
+    angle+=diff;
+    if(angle>180) diff = -diff;   ///大於180回彈
+    if(angle<0) diff = -diff;   ///小於0回彈
+    display();
+}
+int main(int argc,char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("Week14_08160130");
+
+    glutDisplayFunc(display);
+    glutTimerFunc(5000,timer,0);   ///(要等多久,叫誰,參數) 第一個鬧鐘
+
+    glutMainLoop();
+
+}
