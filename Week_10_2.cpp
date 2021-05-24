2a708399af4010e6269e331185fccae051e18732
08160130 <80247079+08160130@users.noreply.github.com>
2021-05-24 14:13:01 +0800

    Create Week_10_2.cpp

---
+++ Week_10_2.cpp
@@ -0,0 +1,34 @@
+#include <windows.h>
+#include <GL/glut.h>
+#include <stdio.h>
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glutSolidTeapot(0.3);
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char key , int x , int y )
+{
+    if(key=='1') PlaySound("do.wav",NULL,SND_ASYNC);
+    if(key=='2') PlaySound("re.wav",NULL,SND_ASYNC);
+    if(key=='3') PlaySound("mi.wav",NULL,SND_ASYNC);
+}
+
+void mouse(int button,int state,int x,int y)
+{
+    if(state==GLUT_DOWN) PlaySound("shot.wav",NULL,SND_ASYNC);
+}
+
+int main(int argc,char **argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("08160130");
+
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutMouseFunc(mouse);
+    glutMainLoop();
+}
