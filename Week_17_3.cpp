81c973e97568c04492f9959f23298e7bf654c363
08160130 <80247079+08160130@users.noreply.github.com>
2021-06-14 13:48:29 +0800

    Update Week_17_3.cpp

--- Week_17_3.cpp
+++ Week_17_3.cpp
@@ -3,7 +3,7 @@ void mouse(int button, int state, int x, int y){
     oldX = x;
 }
 
-void motoin( int x, int y ){
+void motion( int x, int y ){
     angle += x-oldX;
     oldX = x;//小心,不要漏掉了
     glutPostRedisplay();///重畫畫面
